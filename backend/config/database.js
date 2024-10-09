e3e3cc73e6631eb9b28d5170a8f0068cbd8cac4f
Felix Kojo <felixkwadwo300@gmail.com>
2024-10-09 17:46:26 +0000

    directory error v2

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    mongoose.set("strictQuery", false);
+
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
